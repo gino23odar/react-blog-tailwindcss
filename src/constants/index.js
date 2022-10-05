b1e4f447c67d07662bd99d7c78a241c5e57d0343
Gino <ginoalodar@gmail.com>
2022-10-04 22:28:03 -0500

    gave Navbar a structure and added temp logo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,12 @@
 
-import {logo1} from "../assets";
+import {twitter} from "../assets";
 
 export const navLinks = [
   {id: 'inicio',
   title: 'Inicio'},
+  {id: 'info',
+  title: 'Info'},
+  {id: 'extra',
+  title: 'Extra'},
   
-]
+];
