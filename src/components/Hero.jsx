5f756e7073c9366bc6c89269db4f361a071bd088
Gino <ginoalodar@gmail.com>
2022-10-05 20:01:57 -0500

    started building hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import styles from '../style';
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+  <section id='home' className={` flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className='text-white'>top-message:</span>
+            JUAT
+        </p>
+      </div>
+    </div>
+  </section>
+)
 
 export default Hero
