48581254660b8c43c532f468c7444a6105c14292
Gino <ginoalodar@gmail.com>
2022-10-07 21:08:45 -0500

    added message under title, shield image and gradients to image

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,6 @@ import logo from "./logo.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import twitter from "./twitter.svg";
+import shield from "./shield.png";
 
-export{ logo, menu, close, twitter };
+export{ logo, menu, close, twitter, shield };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import styles from '../style';
+import {shield} from '../assets';
 import Start from './Start'
 
 const Hero = () => (
@@ -18,6 +19,27 @@ const Hero = () => (
           <Start/>
         </div>
       </div>
+      <h1 className='font-poppins font-semibold ss:text-[42px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+        Pocos entienden que pasa por aqui
+      </h1>
+      <p className={`${styles.paragraph} max-w-[450px] mt-5`}>
+        Esto es texto random.Esto es texto random. <br/>
+        Esto es texto random.Esto es texto random. <br/>
+        Esto es texto random.Esto es texto random. <br/>
+        Esto es texto random.Esto es texto random. <br/>
+        Esto es texto random. <br/>
+      </p>
+    </div>
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+    <img src={shield} alt="shield" className="w-1/5 md:w-1/3 lg:w-4/6 relative z-[5]" />
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <Start/>
     </div>
   </section>
 )
